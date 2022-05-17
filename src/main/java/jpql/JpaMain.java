a4ad0b39b0374b65b82b224470e0c8a2db270fdb
minMin <goals1005@naver.com>
2022-05-17 23:33:38 +0900

    조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,12 +15,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member1" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
 /*            List<Member> query = em.createQuery("select m from Member m", Member.class).getResultList();
             TypedQuery<String> query1 = em.createQuery("select m.username from Member m", String.class); //반환타입 명확할 때
@@ -37,10 +42,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            //조인시 올바른? (보기 편한) 쿼리
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            // 1. 조인시 올바른? (보기 편한) 쿼리
 //            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
 
-            //임베디드 타입 가져오기
+            // 2. 임베디드 타입 가져오기
 //            em.createQuery("select o.address from Order o", Address.class).getResultList();
 
    /*         List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
@@ -48,16 +56,45 @@ public class JpaMain {
             MemberDTO memberDTO = result.get(0);
             System.out.println("memberDTO = " + memberDTO.getUsername());*/
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
             //페이징
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+            /*for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member1" + i);
+                member.setAge(i);
+                em.persist(member);
+            }*/
+/*            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
                     .setFirstResult(1)
                     .setMaxResults(10) //데이터 몇개를 가져올건지
                     .getResultList();
             System.out.println("result = " + result.size());
             for (Member member1 : result) {
                 System.out.println("member1 = " + member1);
-            }
+            }*/
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //Join
+            //1. 이너조인(쿼리문에 inner 생략 가능), 2.아웃조인 left outer (outer역시 생략 가능)
+//            String query = "select m from Member m inner join m.team t";
+            //3. 세타 조인 (막 조인) : Team name과 Member username 이 동일한 것 찾아서 출력
+//            String query = "select m from Member m, Team t where m.username = t.name";
+
+            //on절 (join에서는 where
+//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            //on절 세타 조인인 경우
+            String query = "select m from Member m left join Team t on m.username = t.name";
+
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            
+
+            System.out.println("result = " + result.size());
 
             tx.commit();//쓰기지연 SQL 저장소에 SQL문을 DB에 보냄
         } catch (Exception e) {
